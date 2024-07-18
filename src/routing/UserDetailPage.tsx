ddf4a3861b3cf8e69be10da82071fdacdb723fb5
DinelyWellehewage <dinelywh78@gmail.com>
2024-07-18 06:24:48 +0200

    getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,12 @@
+import { log } from "console";
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
+  console.log(searchParams.get("name")); //url?name=Alice
   return <p>User</p>;
 };
 
